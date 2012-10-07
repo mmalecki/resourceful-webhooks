4a149c61b5e73040c58ddc2aa99bba49eb9e7cb9
Maciej Małecki <me@mmalecki.com>
2012-10-08 01:01:13 +0200

    [test] Add a test

---
+++ test/resourceful-webhooks-test.js
@@ -0,0 +1,64 @@
+var http = require('http'),
+    assert = require('assert'),
+    cb = require('assert-called'),
+    resourceful = require('resourceful');
+
+require('../lib/resourceful-webhooks');
+
+var PORT = 8123,
+    gotCallbacks = 0;
+
+function maybeEnd() {
+  if (++gotCallbacks === 2) {
+    server.close();
+  }
+}
+
+var server = http.createServer(function (req, res) {
+  assert.equal(req.url, '/?event=create');
+
+  var data = '';
+
+  req.on('data', function (chunk) {
+    data += chunk;
+  });
+
+  req.on('end', function () {
+    data = JSON.parse(data);
+
+    if (data.hello === 'world') {
+      res.writeHead(200);
+      res.end();
+    }
+    else if (data.hello === 'universe') {
+      res.writeHead(400);
+      res.end();
+    }
+    else {
+      assert(false);
+    }
+  });
+}).listen(PORT);
+
+var Resource = resourceful.define('Resource', function () {
+  this.webhooks({
+    url: 'http://127.0.0.1:' + PORT.toString(),
+    events: ['create']
+  });
+});
+
+Resource.create({
+  id: 'resource/good',
+  hello: 'world'
+}, cb(function goodCb(err) {
+  assert(!err);
+  maybeEnd();
+}));
+
+Resource.create({
+  id: 'resource/bad',
+  hello: 'universe'
+}, cb(function badCb(err) {
+  assert(err);
+  maybeEnd();
+}));
